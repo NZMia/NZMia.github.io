cf2db489a097d05cec9d3113a099b24bc3f22d1b
NZMia <mia7zwy@hotmail.com>
2023-06-29 22:08:02 +1200

    build: set up monorepo

---
+++ client/src/App.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [ testData, setTestData] = useState<string>(''); 
+  useEffect(() => {
+    fetch('/hello')
+      .then((res) => res.json())
+      .then((data) => setTestData(data.data));
+  }, [])
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{testData}</p>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
